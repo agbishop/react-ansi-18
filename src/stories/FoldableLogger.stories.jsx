e3bd164fd9fdb102869b15a39aa25cf8456c19e5
agbishop <andrew.bishop9625@gmail.com>
2023-01-27 23:02:31 -0600

    missing storybook

--- src/stories/FoldableLogger.stories.jsx
+++ src/stories/FoldableLogger.stories.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import FoldableLogger from "../index.js";
 import testdata from "./data.json";
+import { AnsiLog } from "../index";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Logger/FoldableLogger",
-  component: FoldableLogger,
+  component: AnsiLog,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 };
@@ -13,7 +13,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => (
   <div style={{ height: `500px`, width: `100%`, overflow: "auto" }}>
-    <FoldableLogger {...args} />
+    <AnsiLog {...args} />
   </div>
 );
 
